809ebb3745324e8e9c6aa0ca20011900a5b7318c
Julia balashova <yulya.balashova.05@mail.ru>
2022-11-21 18:19:58 +0300

    fixed naming to lowercamelcase

--- src/Kartana.java
+++ src/Kartana.java
@@ -7,10 +7,10 @@ public class Kartana extends Pokemon {
         setType(Type.GRASS, Type.STEEL);
         setStats(59, 181, 131, 59, 31, 109);
         IronDefense ironDefense = new IronDefense(0, 0);
-        MegaDrain MegaDrain = new MegaDrain(40, 100);
+        MegaDrain megaDrain = new MegaDrain(40, 100);
         BrutalSwing BrutalSwing = new BrutalSwing(60, 100);
         FlameCharge FlameCharge = new FlameCharge(50, 100);
-        setMove(ironDefense, MegaDrain, BrutalSwing, FlameCharge);
+        setMove(ironDefense, megaDrain, BrutalSwing, FlameCharge);
     }
 
 }
